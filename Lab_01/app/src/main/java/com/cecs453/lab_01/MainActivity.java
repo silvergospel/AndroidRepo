f5e99efd5c759e041601aa4750d9b92c01ee9aa8
Michael Williams <simianmonk@yahoo.com>
2015-09-07 19:24:49 -0700

    Assignment complete

--- Lab_01/app/src/main/java/com/cecs453/lab_01/MainActivity.java
+++ Lab_01/app/src/main/java/com/cecs453/lab_01/MainActivity.java
@@ -1,12 +1,31 @@
 package com.cecs453.lab_01;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    public final static String EXTRA_MESSAGE = "com.cecs453.lab_01.MESSAGE";
+    public void sendMessage(View view){
+        //// TODO: 9/7/2015
+        Intent intent = new Intent(this, DisplayActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        /*
+        //Direct route within single activity
+        ((TextView)findViewById(R.id.helloText)).setText("Hello, " + message);
+        */
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
