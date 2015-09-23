0533a0a938c49ad2ee709687ec2a2ab24e8310a0
Michael Williams <simianmonk@yahoo.com>
2015-09-23 10:33:16 -0700

    Lab 3 complete

--- Lab_03/app/src/main/java/com/cecs453/lab_03/MainActivity.java
+++ Lab_03/app/src/main/java/com/cecs453/lab_03/MainActivity.java
@@ -4,13 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    List<Map<String,String>> planetsList = new ArrayList<Map<String,String>>();
+    SimpleAdapter simpleAdpt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initList();
+
+        ListView lv = (ListView) findViewById(R.id.listView);
+        simpleAdpt = new SimpleAdapter(this, planetsList, android.R.layout.simple_list_item_1, new String[] {"planet"}, new int[] {android.R.id.text1});
+        lv.setAdapter(simpleAdpt);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView clickedView = (TextView) view;
+                Toast.makeText(MainActivity.this,"Item with id [" +
+                        id + "] - Position [" +
+                        position + "] - Planet [" +
+                        clickedView.getText() + "]", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -33,5 +62,22 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initList() {
+        planetsList.add(createPlanet("planet","Mercury"));
+        planetsList.add(createPlanet("planet","Venus"));
+        planetsList.add(createPlanet("planet","Earth"));
+        planetsList.add(createPlanet("planet","Mars"));
+        planetsList.add(createPlanet("planet","Jupiter"));
+        planetsList.add(createPlanet("planet","Saturn"));
+        planetsList.add(createPlanet("planet","Uranus"));
+        planetsList.add(createPlanet("planet","Neptune"));
+    }
+
+    private HashMap<String,String> createPlanet(String key, String name) {
+        HashMap<String, String> planet = new HashMap<String,String>();
+        planet.put(key,name);
+        return planet;
     }
 }
